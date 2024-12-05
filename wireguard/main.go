bdb9ee571c27a7f3041adf3dc763b6cf7c8542f4
nalek0 <belash.aleksandr777@gmail.com>
2024-12-05 22:55:54 +0300

    Added wireguard interface

--- wireguard/main.go
+++ wireguard/main.go
@@ -16,6 +16,10 @@ type connection interface {
 	turn_off(interface_name string)
 }
 
+type Wireguard struct {
+	config_path string
+}
+
 type AmneziaWG struct {
 	config_path string
 }
@@ -86,6 +90,74 @@ func (wg AmneziaWG) turn_off(interface_name string) {
 	}
 }
 
+// Wireguard:
+
+func (wg Wireguard) run_interface(interface_name string) {
+	var cmd = exec.Command("sudo", "./libs/wireguard-go", interface_name)
+	if err := cmd.Run(); err != nil {
+		fmt.Printf("Could not run Wireguard interface: %s\n", err)
+		wg.turn_off(interface_name)
+		os.Exit(1)
+	} else {
+		fmt.Printf("+ Ran Wireguard interface\n")
+	}
+}
+
+func (wg Wireguard) set_config(interface_name string) {
+	var cmd = exec.Command("sudo", "./libs/wg", "setconf", interface_name, config_path)
+	if output, err := cmd.CombinedOutput(); err != nil {		
+		fmt.Printf("Could not configure Wireguard interface: %s\n", err)
+		fmt.Printf("Output: %s\n", string(output))
+		wg.turn_off(interface_name)
+		os.Exit(1)
+	} else {
+		fmt.Printf("+ Set config\n")
+	}
+}
+
+func (wg Wireguard) add_address(interface_name string, address string) {
+	var cmd = exec.Command("sudo", "ip", "-4", "address", "add", address, "dev", interface_name)
+	if err := cmd.Run(); err != nil {		
+		fmt.Printf("Could not add address %s: %s\n", address, err)
+		wg.turn_off(interface_name)
+		os.Exit(1)
+	} else {
+		fmt.Printf("+ Add %s address\n", address)
+	}
+}
+
+func (wg Wireguard) set_mtu(interface_name string, mtu string) {
+	var cmd = exec.Command("sudo", "ip", "link", "set", "mtu", mtu, "up", "dev", interface_name)
+	if err := cmd.Run(); err != nil {		
+		fmt.Printf("Could not set up mtu %s: %s\n", mtu, err)
+		wg.turn_off(interface_name)
+		os.Exit(1)
+	} else {
+		fmt.Printf("+ Set %s mtu\n", mtu)
+	}
+}
+
+func (wg Wireguard) turn_on(interface_name string) {
+	wg.run_interface(interface_name)
+	wg.set_config(interface_name)
+	wg.add_address(interface_name, ADDRESS)
+	wg.set_mtu(interface_name, MTU)
+	// Set dns
+	wg.add_address(interface_name, ALLOWED_IP)
+}
+
+func (wg Wireguard) turn_off(interface_name string) {
+	var cmd = exec.Command("sudo", "ip", "link", "del", interface_name)
+	if output, err := cmd.CombinedOutput(); err != nil {		
+		fmt.Printf("Could not remove Wireguard interface: %s\n", err)
+		fmt.Printf("Output: %s\n", string(output))
+		wg.turn_off(interface_name)
+		os.Exit(1)
+	} else {
+		fmt.Printf("+ Removed interface\n")
+	}
+}
+
 const CONGIG_PATH_DEFAULT = "/etc/amnezia/amneziawg/"
 const CONGIG_PATH_USAGE = "Path to the config"
 const TYPE_DEFAULT = "amneziawg"
@@ -103,7 +175,7 @@ func main() {
 	
     fmt.Printf("Parameters: %s, %s\n", config_path, connection_type)
 
-	var amneziawg = AmneziaWG { config_path }
-	amneziawg.turn_on("awg0")
-	amneziawg.turn_off("awg0")
+	var wireguard = Wireguard { config_path }
+	wireguard.turn_on("wg0")
+	wireguard.turn_off("wg0")
 }
--- wireguard/prepare_libs.sh
+++ wireguard/prepare_libs.sh
@@ -3,13 +3,19 @@ rm -rf libs/
 cd wireguard-go/
 make
 cd ../
+cd wireguard-tools/src/
+make
+cd ../../
+
 cd amneziawg-go/
 make
 cd ../
 cd amneziawg-tools/src/
 make
 cd ../../
+
 mkdir libs
 cp wireguard-go/wireguard-go libs/
+cp wireguard-tools/src/wg libs/wg
 cp amneziawg-go/amneziawg-go libs/
 cp amneziawg-tools/src/wg libs/awg
