c803f5bb6b80aeb61a4d6bbd8762a4fa5625a9df
Aleksandr Belash <belash.aleksandr777@gmail.com>
2024-12-09 12:14:06 +0300

    Linux part refactoring

--- wireguard/linux/tunnel_config.go
+++ wireguard/linux/tunnel_config.go
@@ -6,14 +6,8 @@ import (
 	"net"
 	"os"
 	"os/exec"
-	"os/signal"
 
-	"golang.org/x/sys/unix"
-
-	"github.com/amnezia-vpn/amneziawg-go/conn"
 	"github.com/amnezia-vpn/amneziawg-go/device"
-	"github.com/amnezia-vpn/amneziawg-go/ipc"
-	"github.com/amnezia-vpn/amneziawg-go/tun"
 	"github.com/vishvananda/netlink"
 )
 
@@ -118,118 +112,8 @@ func tunnelAmneziaWGOff(interfaceName string) {
 	var cmd = exec.Command("sudo", "ip", "link", "del", interfaceName)
 
 	if output, err := cmd.CombinedOutput(); err != nil {
-		log.Fatal("Could not remove AmneziaWG interface:\n error: %s\n output: %s\n", err, output)
+		log.Fatalf("Could not remove AmneziaWG interface:\n error: %s\n output: %s\n", err, output)
 	} else {
 		log.Println("Removed AmneziaWG interface")
 	}
-}
-
-func installTunnel(configPath string, interfaceName string) {
-	// Logger definition:
-	logLevel := device.LogLevelVerbose
-
-	logger := device.NewLogger(
-		logLevel,
-		fmt.Sprintf("(%s) ", interfaceName),
-	)
-
-	// open TUN device (or use supplied fd)
-	tdev, err := tun.CreateTUN(interfaceName, device.DefaultMTU)
-
-	logger.Verbosef("Starting amneziawg")
-
-	if err == nil {
-		realInterfaceName, err2 := tdev.Name()
-		if err2 == nil {
-			interfaceName = realInterfaceName
-		}
-	} else {
-		logger.Errorf("Failed to create TUN device: %v", err)
-		os.Exit(1)
-	}
-
-	// open UAPI file
-	fileUAPI, err := ipc.UAPIOpen(interfaceName)
-
-	if err != nil {
-		logger.Errorf("UAPI listen error: %v", err)
-		os.Exit(1)
-	}
-
-	// Start device:
-
-	device := device.NewDevice(tdev, conn.NewDefaultBind(), logger)
-
-	logger.Verbosef("Device started")
-
-	errs := make(chan error)
-	term := make(chan os.Signal, 1)
-
-	uapi, err := ipc.UAPIListen(interfaceName, fileUAPI)
-
-	if err != nil {
-		logger.Errorf("Failed to listen on uapi socket: %v", err)
-		os.Exit(1)
-	}
-
-	go func() {
-		// Extra ipc configuration:
-		for {
-			conn, err := uapi.Accept()
-			if err != nil {
-				errs <- err
-				return
-			}
-			go device.IpcHandle(conn)
-		}
-	}()
-
-	logger.Verbosef("UAPI listener started")
-
-	// Configure AmneziaWG
-
-	configureAmneziaWG(device, configPath)
-	postConfigAmneziaWg(interfaceName)
-
-	// wait for program to terminate
-
-	signal.Notify(term, unix.SIGTERM)
-	signal.Notify(term, os.Interrupt)
-
-	select {
-	case <-term:
-	case <-errs:
-	case <-device.Wait():
-	}
-
-	// clean up
-
-	tunnelAmneziaWGOff(interfaceName)
-	uapi.Close()
-	device.Close()
-
-	logger.Verbosef("Shutting down")
-}
-
-func printUnixUsage() {
-	fmt.Printf("Usage: %s [intalltunnelservice CONFIG_PATH INTERFACE_NAME]\n", os.Args[0])
-	os.Exit(1)
-}
-
-func main() {
-	if len(os.Args) < 2 {
-		printUnixUsage()
-	}
-
-	switch os.Args[1] {
-	case "installtunnelservice":
-		if len(os.Args) != 4 {
-			printUnixUsage()
-		}
-
-		installTunnel(os.Args[2], os.Args[3])
-		os.Exit(0)
-	}
-
-	printUnixUsage()
 }
